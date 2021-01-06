dfa0cc2fa85a4b732f33ae9c2d37273aaf392efc
killian <killian.gay28@gmail.com>
2021-01-06 09:44:58 +0100

    add animation

---
+++ dieselite/Assets/script/player/playerMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    private bool isMoving;
+    private Vector2 input;
+
+    private Animator animator;
+
+    private void Awake()
+    { 
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if(!isMoving)
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+            if (input != Vector2.zero)
+            {
+                animator.SetFloat("moveX", input.x);
+                animator.SetFloat("moveY", input.y);
+
+                var targetPosition = transform.position;
+                targetPosition.x += input.x;
+                targetPosition.y += input.y;
+
+                StartCoroutine(Move(targetPosition));
+            }
+        }
+
+        animator.SetBool("isMoving", isMoving);
+    }
+
+    IEnumerator Move(Vector3 targetPosition)
+    {
+        isMoving = true;
+
+        while ((targetPosition - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPosition;
+
+        isMoving = false;
+    }
+}
